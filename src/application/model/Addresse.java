62b7068cab0a2cbb7a2c5311986716a203063c5e
Bjarke-Arnved <eaa23bjar@students.eaaa.dk>
2024-05-07 09:58:38 +0200

    added Addresse

--- src/application/model/Addresse.java
+++ src/application/model/Addresse.java
@@ -1,5 +1,15 @@
 package application.model;
 
 public class Addresse {
+    private String gadeNummer;
+    private String gadeNavn;
+    private String postnummer;
+    private String land;
 
+    public Addresse(String gadeNummer, String gadeNavn, String postnummer, String land) {
+        this.gadeNummer = gadeNummer;
+        this.gadeNavn = gadeNavn;
+        this.postnummer = postnummer;
+        this.land = land;
+    }
 }
