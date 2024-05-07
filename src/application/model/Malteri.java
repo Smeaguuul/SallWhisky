ad4c79fd7b31eeca2f2cd4e9c249c1a8c78958f5
Smeaguuul <mikkelhoffmannp@gmail.com>
2024-05-07 10:26:24 +0200

    Tilføjede nogle metoder i Controller

--- src/application/model/Malteri.java
+++ src/application/model/Malteri.java
@@ -4,4 +4,10 @@ public class Malteri {
     private String navn;
     private String beskrivelse;
     private Addresse addresse;
+
+    public Malteri(String navn, String beskrivelse, Addresse addresse) {
+        this.navn = navn;
+        this.beskrivelse = beskrivelse;
+        this.addresse = addresse;
+    }
 }
