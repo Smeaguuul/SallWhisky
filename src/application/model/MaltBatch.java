90493262c6e74f3426ebd2226496a1f486e728e3
ThorB <eaa23thbj@students.eaaa.dk>
2024-05-07 10:16:04 +0200

    Storage, Maltbatch og kornsort

---
+++ src/application/model/MaltBatch.java
@@ -0,0 +1,19 @@
+package application.model;
+
+import java.time.LocalDate;
+
+public class MaltBatch {
+    private Kornsort kornsort;
+    private int nummer;
+    private LocalDate ankomstDato;
+    private Malteri malteri;
+    private Mark mark;
+
+    public MaltBatch(Kornsort kornsort, int nummer, LocalDate ankomstDato, Malteri malteri, Mark mark) {
+        this.kornsort = kornsort;
+        this.nummer = nummer;
+        this.ankomstDato = ankomstDato;
+        this.malteri = malteri;
+        this.mark = mark;
+    }
+}
