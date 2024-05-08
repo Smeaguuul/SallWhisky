94422efb189f30ccf7180a742008d6acd0dfc5cb
Smeaguuul <mikkelhoffmannp@gmail.com>
2024-05-08 11:16:07 +0200

    Slut 07/05

---
+++ test/application/controller/ControllerTest.java
@@ -0,0 +1,20 @@
+package application.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ControllerTest {
+
+    @Test
+    void getMedarbejder() {
+    }
+
+    @Test
+    void opretFad() {
+    }
+
+    @Test
+    void opretDestillat() {
+    }
+}
