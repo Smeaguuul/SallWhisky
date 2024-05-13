72afb679b67547b90aefce1aeb1b0f8bc6ddc6a6
Bjarke-Arnved <eaa23bjar@students.eaaa.dk>
2024-05-13 11:22:29 +0200

    opret medarbjeder metode i controller og rettelse i medarbejder

--- src/application/model/Medarbejder.java
+++ src/application/model/Medarbejder.java
@@ -1,10 +1,19 @@
 package application.model;
 
 public class Medarbejder {
+    private static int antalMedarbejder = 0;
     private int nummer;
     private String navn;
     private String cpr;
     private String signatur;
+
+    public Medarbejder(String navn, String cpr, String signatur) {
+        antalMedarbejder++;
+        this.nummer = antalMedarbejder;
+        this.navn = navn;
+        this.cpr = cpr;
+        this.signatur = signatur;
+    }
 
     public Medarbejder(int nummer, String navn, String cpr, String signatur) {
         this.nummer = nummer;
